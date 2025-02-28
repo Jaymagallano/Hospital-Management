dec11ec8409a702f8ecad0ddcf32e34d07ffa495
Jaymagallano <jaymagallano2004@gmail.com>
2025-02-28 16:59:32 +0800

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use illuminate\Support\Facades\Auth;
+
+use App\Model\User;
+
+
+class HomeController extends Controller
+{
+    public function redirect ()
+    {
+
+    if(Auth::id())
+    {
+        if(Auth::user()->usertype=='0')
+        {
+            return view('user.home');
+        }
+
+        else
+
+        {
+            return view('admin.home');
+        }
+    }
+        else
+        {
+            return redirect()->back();
+        }
+    }
+
+    public function index()
+    {
+        return view('user.home');
+    }
+}
